d07c89ba234a94393e17fda00587e6044f95f63c
Erdem <erdemsincer123@icloud.com>
2025-05-01 00:38:47 +0300

    entity ve contex ayarları

---
+++ CourseService/Data/CourseDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CourseService.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CourseService.Data
+{
+    public class CourseDbContext : DbContext
+    {
+        public CourseDbContext(DbContextOptions<CourseDbContext> options) : base(options) { }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
---
+++ CourseService/Entities/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace CourseService.Entities
+{
+    public class Course
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid OwnerId { get; set; } // JWT'den alınacak
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
