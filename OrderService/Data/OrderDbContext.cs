d174650476f21f5ec42a70645d02e8906c31ce86
Erdem <erdemsincer123@icloud.com>
2025-05-01 01:17:44 +0300

    entity ve contex ayarlaqrı

---
+++ OrderService/Data/OrderDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Entities;
+using System.Collections.Generic;
+
+namespace OrderService.Data
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options) { }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ OrderService/Entities/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderService.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public Guid UserId { get; set; }     // Teklifi onaylayan kişi
+        public Guid CourseId { get; set; }
+        public decimal Amount { get; set; }
+        public bool IsPaid { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
