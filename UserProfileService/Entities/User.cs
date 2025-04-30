dfce881df93db1278113440bd786b881f7de037a
Erdem <erdemsincer123@icloud.com>
2025-04-30 15:22:16 +0300

    user skill entity

---
+++ UserProfileService/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace UserProfileService.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; } = null!;
+        public ICollection<UserSkill> Skills { get; set; } = new List<UserSkill>();
+    }
+}
---
+++ UserProfileService/Entities/UserSkill.cs
@@ -0,0 +1,18 @@
+﻿namespace UserProfileService.Entities
+{
+    public class UserSkill
+    {
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public string SkillName { get; set; } = null!;
+        public SkillType SkillType { get; set; }
+
+        public User User { get; set; } = null!;
+    }
+
+    public enum SkillType
+    {
+        Known,   // Bildiği
+        Wanted   // Öğrenmek istediği
+    }
+}
