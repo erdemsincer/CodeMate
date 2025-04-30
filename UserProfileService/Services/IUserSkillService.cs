b740b6afdae9599fed4535a638640a82c33f5ffa
Erdem <erdemsincer123@icloud.com>
2025-04-30 15:22:24 +0300

    Servisler

---
+++ UserProfileService/Services/IUserSkillService.cs
@@ -0,0 +1,11 @@
+﻿using UserProfileService.Dtos;
+
+namespace UserProfileService.Services
+{
+    public interface IUserSkillService
+    {
+        Task AddSkillAsync(UserSkillDto dto);
+        Task<List<UserSkillDto>> GetSkillsAsync(Guid userId);
+        Task DeleteSkillAsync(int skillId);
+    }
+}
---
+++ UserProfileService/Services/UserSkillService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserProfileService.Data;
+using UserProfileService.Dtos;
+using UserProfileService.Entities;
+
+namespace UserProfileService.Services
+{
+    public class UserSkillService : IUserSkillService
+    {
+        private readonly UserProfileDbContext _context;
+
+        public UserSkillService(UserProfileDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddSkillAsync(UserSkillDto dto)
+        {
+            var skill = new UserSkill
+            {
+                UserId = dto.UserId,
+                SkillName = dto.SkillName,
+                SkillType = Enum.Parse<SkillType>(dto.SkillType, true)
+            };
+
+            _context.UserSkills.Add(skill);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<UserSkillDto>> GetSkillsAsync(Guid userId)
+        {
+            return await _context.UserSkills
+                .Where(s => s.UserId == userId)
+                .Select(s => new UserSkillDto
+                {
+                    UserId = s.UserId,
+                    SkillName = s.SkillName,
+                    SkillType = s.SkillType.ToString()
+                })
+                .ToListAsync();
+        }
+
+        public async Task DeleteSkillAsync(int skillId)
+        {
+            var skill = await _context.UserSkills.FindAsync(skillId);
+            if (skill is not null)
+            {
+                _context.UserSkills.Remove(skill);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
