9425994bafaf0984d41719afafc90093648af24f
Erdem <erdemsincer123@icloud.com>
2025-04-30 16:09:03 +0300

    Dto

--- UserProfileService/Dtos/UserSkillDto.cs
+++ UserProfileService/Dtos/UserSkillDto.cs
@@ -2,7 +2,7 @@
 {
     public class UserSkillDto
     {
-        public Guid UserId { get; set; }
+       
         public string SkillName { get; set; } = null!;
         public string SkillType { get; set; } = null!; // "Known" veya "Wanted"
     }
