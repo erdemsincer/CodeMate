798c67648361801ec53a60ad1276eaf0155d67dc
Erdem <erdemsincer123@icloud.com>
2025-05-01 01:18:50 +0300

    RabbitMQ AYARLARI

---
+++ OrderService/Consumers/OfferApprovedConsumer.cs
@@ -0,0 +1,34 @@
+﻿using MassTransit;
+using OrderService.Data;
+using OrderService.Entities;
+using OrderService.Events;
+
+namespace OrderService.Consumers
+{
+    public class OfferApprovedConsumer : IConsumer<OfferApprovedEvent>
+    {
+        private readonly OrderDbContext _context;
+
+        public OfferApprovedConsumer(OrderDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Consume(ConsumeContext<OfferApprovedEvent> context)
+        {
+            var message = context.Message;
+
+            var order = new Order
+            {
+                UserId = message.UserId,
+                CourseId = message.CourseId,
+                Amount = message.OfferedPrice,
+                CreatedAt = DateTime.UtcNow,
+                IsPaid = false
+            };
+
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
