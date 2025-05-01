2f0dbc4db35532326bd7480e9d32055cf066b901
Erdem <erdemsincer123@icloud.com>
2025-05-01 03:09:40 +0300

    payment servicede onaylama

---
+++ PaymentService/Consumers/PaymentRequestedConsumer.cs
@@ -0,0 +1,32 @@
+﻿using CodeMate.Shared.Contracts;
+using MassTransit;
+
+namespace PaymentService.Consumers;
+
+public class PaymentRequestConsumer : IConsumer<PaymentRequestedEvent>
+{
+    private readonly IPublishEndpoint _publishEndpoint;
+
+    public PaymentRequestConsumer(IPublishEndpoint publishEndpoint)
+    {
+        _publishEndpoint = publishEndpoint;
+    }
+
+    public async Task Consume(ConsumeContext<PaymentRequestedEvent> context)
+    {
+        var message = context.Message;
+
+        Console.WriteLine($"💰 Payment received for OrderId: {message.OrderId}");
+
+        // Simüle edilmiş ödeme (gerçek sistemde Stripe, iyzico vb. entegre edilir)
+        await Task.Delay(1000);
+
+        await _publishEndpoint.Publish(new PaymentCompletedEvent
+        {
+            OrderId = message.OrderId,
+            IsSuccessful = true
+        });
+
+        Console.WriteLine($"✅ Payment completed published for OrderId: {message.OrderId}");
+    }
+}
--- PaymentService/Program.cs
+++ PaymentService/Program.cs
@@ -1,23 +1,81 @@
-var builder = WebApplication.CreateBuilder(args);
+using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using PaymentService.Consumers;
+using System.Text;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+// JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = config["JwtSettings:Issuer"],
+        ValidAudience = config["JwtSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtSettings:SecretKey"]!))
+    };
+});
+
+// MassTransit
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<PaymentRequestConsumer>();
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("rabbitmq", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+
+        cfg.ReceiveEndpoint("payment-request-queue", e =>
+        {
+            e.ConfigureConsumer<PaymentRequestConsumer>(context);
+        });
+    });
+});
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "PaymentService", Version = "v1" });
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header
+    });
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
