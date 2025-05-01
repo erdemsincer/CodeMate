9593465c16e67a41af4f280e80d826f35e7f1559
Erdem <erdemsincer123@icloud.com>
2025-05-01 17:35:17 +0300

    bildirim işlemi

---
+++ NotificationService/Consumers/PaymentCompletedConsumer.cs
@@ -0,0 +1,18 @@
+﻿using CodeMate.Shared.Contracts;
+using MassTransit;
+
+namespace NotificationService.Consumers
+{
+    public class PaymentCompletedConsumer : IConsumer<PaymentCompletedEvent>
+    {
+        public async Task Consume(ConsumeContext<PaymentCompletedEvent> context)
+        {
+            var message = context.Message;
+
+            // Burada mail atabilir ya da loglayabilirsin.
+            Console.WriteLine($"[Notification] Payment successful for OrderId: {message.OrderId}");
+
+            await Task.CompletedTask;
+        }
+    }
+}
--- NotificationService/Program.cs
+++ NotificationService/Program.cs
@@ -1,25 +1,37 @@
+using MassTransit;
+using NotificationService.Consumers;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<PaymentCompletedConsumer>();
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("rabbitmq", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+
+        cfg.ReceiveEndpoint("notification-service", e =>
+        {
+            e.ConfigureConsumer<PaymentCompletedConsumer>(context);
+        });
+    });
+});
+
+builder.Services.AddMassTransitHostedService();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
