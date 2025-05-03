71379f934abc028a4be38676f17e277a82558206
Erdem <erdemsincer123@icloud.com>
2025-05-03 04:33:57 +0300

    review entity

---
+++ ReviewService/Entities/Review.cs
@@ -0,0 +1,12 @@
+﻿namespace ReviewService.Entities
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public Guid ReviewerId { get; set; } // Yorumu yapan kullanıcı
+        public Guid RevieweeId { get; set; } // Yorumu alan kullanıcı
+        public string Content { get; set; } = null!;
+        public int Rating { get; set; } // 1 - 5
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
