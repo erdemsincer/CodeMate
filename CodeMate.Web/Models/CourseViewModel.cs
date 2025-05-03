73d9c0e5395f06dcc068a73cd2effcfd83db9f87
Erdem <erdemsincer123@icloud.com>
2025-05-03 05:20:56 +0300

    teklif verme sayfası

---
+++ CodeMate.Web/Models/CourseViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeMate.Web.Models
+{
+    public class CourseViewModel
+    {
+        public Guid Id { get; set; }
+        public Guid OwnerId { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ CodeMate.Web/Models/OfferCreateViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeMate.Web.Models
+{
+    public class OfferCreateViewModel
+    {
+        public Guid CourseId { get; set; }
+        public decimal OfferedPrice { get; set; }
+    }
+}
