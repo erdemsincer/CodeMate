1d50273b2580e2187918f54477bb5d4d71285653
Erdem <erdemsincer123@icloud.com>
2025-05-04 00:21:49 +0300

    order controller

---
+++ CodeMate.Web/Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using CodeMate.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace CodeMate.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public OrderController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> MyOrders()
+        {
+            var token = HttpContext.Session.GetString("access_token");
+            if (string.IsNullOrEmpty(token))
+                return RedirectToAction("Login", "Auth");
+
+            var client = _httpClientFactory.CreateClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.GetAsync("http://orderservice:8080/api/orders/my");
+            var json = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+                return View(new List<OrderViewModel>());
+
+            var orders = JsonSerializer.Deserialize<List<OrderViewModel>>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            return View(orders);
+        }
+    }
+}
