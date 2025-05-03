59f3d36e521b4d5664fad45e010b49608ce6312f
Erdem <erdemsincer123@icloud.com>
2025-05-03 05:03:21 +0300

    viewmodeller

---
+++ CodeMate.Web/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeMate.Web.Models
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ CodeMate.Web/Models/RegisterViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeMate.Web.Models
+{
+    public class RegisterViewModel
+    {
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+
+}
