ae458491d97378adb80e012e2c52a2f0168f9a23
Erdem <erdemsincer123@icloud.com>
2025-05-03 03:40:41 +0300

    Dto

---
+++ ChatService/Dtos/ChatMessageDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatService.Dtos
+{
+    public class ChatMessageDto
+    {
+        public Guid SenderId { get; set; }
+        public Guid ReceiverId { get; set; }
+        public string Content { get; set; } = null!;
+        public DateTime SentAt { get; set; }
+    }
+}
---
+++ ChatService/Dtos/SendMessageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatService.Dtos
+{
+    public class SendMessageDto
+    {
+        public Guid ReceiverId { get; set; }
+        public string Content { get; set; } = null!;
+    }
+}
