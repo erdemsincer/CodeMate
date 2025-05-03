c0ffa71d16b34219ada774e01504b79af0a2ab53
Erdem <erdemsincer123@icloud.com>
2025-05-04 00:14:34 +0300

    order view model

---
+++ CodeMate.Web/Models/OrderViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeMate.Web.Models
+{
+    public class OrderViewModel
+    {
+        public int Id { get; set; }
+        public Guid CourseId { get; set; }
+        public string CourseTitle { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+        public bool IsPaid { get; set; }
+    }
+}
