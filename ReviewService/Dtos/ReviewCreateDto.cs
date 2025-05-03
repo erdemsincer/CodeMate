a8731e065ecbea86e77fba690a8cd06f344a6d92
Erdem <erdemsincer123@icloud.com>
2025-05-03 04:19:17 +0300

    Review dto

---
+++ ReviewService/Dtos/ReviewCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ReviewService.Dtos
+{
+    public class ReviewCreateDto
+    {
+        public Guid RevieweeId { get; set; }
+        public string Content { get; set; } = null!;
+        public int Rating { get; set; }
+    }
+}
---
+++ ReviewService/Dtos/ReviewDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ReviewService.Dtos
+{
+    public class ReviewDto
+    {
+        public Guid ReviewerId { get; set; }
+        public Guid RevieweeId { get; set; }
+        public string Content { get; set; } = null!;
+        public int Rating { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
