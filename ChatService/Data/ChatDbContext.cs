2cca2d2bc4a187c87c8eb3cf82278fd15b69a7f0
Erdem <erdemsincer123@icloud.com>
2025-05-03 03:34:56 +0300

    chat service

---
+++ ChatService/Data/ChatDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ChatService.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ChatService.Data
+{
+    public class ChatDbContext : DbContext
+    {
+        public ChatDbContext(DbContextOptions<ChatDbContext> options) : base(options) { }
+
+        public DbSet<ChatMessage> ChatMessages { get; set; }
+    }
+}
---
+++ ChatService/Entities/ChatMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatService.Entities
+{
+    public class ChatMessage
+    {
+        public int Id { get; set; }
+        public Guid SenderId { get; set; }
+        public Guid ReceiverId { get; set; }
+        public string Message { get; set; } = null!;
+        public DateTime SentAt { get; set; } = DateTime.UtcNow;
+    }
+}
