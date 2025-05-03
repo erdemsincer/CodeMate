ebf38feeb9a8d36132e2d822ed93260fd3a3f6db
Erdem <erdemsincer123@icloud.com>
2025-05-03 05:11:43 +0300

    kursları listeleme

--- CodeMate.Web/Controllers/DashboardController.cs
+++ CodeMate.Web/Controllers/DashboardController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CodeMate.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace CodeMate.Web.Controllers
 {
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DashboardController(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var token = HttpContext.Session.GetString("access_token");
+            if (string.IsNullOrEmpty(token))
+                return RedirectToAction("Login", "Auth");
+
+            var client = _httpClientFactory.CreateClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.GetAsync("http://courseservice:8080/api/courses");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                ViewBag.Error = "Kurslar getirilemedi.";
+                return View(new List<CourseViewModel>());
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            var courses = JsonSerializer.Deserialize<List<CourseViewModel>>(content, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            return View(courses);
         }
     }
 }
