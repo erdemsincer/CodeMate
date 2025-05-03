2c9c936214dc83918563ee13c82ecf6d7eaab686
Erdem <erdemsincer123@icloud.com>
2025-05-04 00:08:36 +0300

    uıde kurs title görme

--- CodeMate.Web/Models/OfferViewModel.cs
+++ CodeMate.Web/Models/OfferViewModel.cs
@@ -4,8 +4,10 @@
     {
         public int Id { get; set; }
         public Guid CourseId { get; set; }
+        public string CourseTitle { get; set; } = null!;
         public decimal OfferedPrice { get; set; }
         public bool IsApproved { get; set; }
         public DateTime CreatedAt { get; set; }
     }
+
 }
