717be4394899bc0ed47f03c03574a3b26042ba5e
Erdem <erdemsincer123@icloud.com>
2025-05-03 04:06:39 +0300

    signalr

---
+++ ChatService/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatService.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(Guid senderId, Guid receiverId, string content)
+        {
+            // Belirli kullanıcıya mesaj gönder
+            await Clients.User(receiverId.ToString()).SendAsync("ReceiveMessage", senderId, content);
+        }
+    }
+}
