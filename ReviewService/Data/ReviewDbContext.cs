74f2cf3935fce5c2531a1675ec911789bf4ad792
Erdem <erdemsincer123@icloud.com>
2025-05-03 04:34:05 +0300

    context

---
+++ ReviewService/Data/ReviewDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReviewService.Entities;
+
+namespace ReviewService.Data
+{
+    public class ReviewDbContext : DbContext
+    {
+        public ReviewDbContext(DbContextOptions<ReviewDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Review> Reviews => Set<Review>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Review>(entity =>
+            {
+                entity.HasKey(r => r.Id);
+                entity.Property(r => r.Content).IsRequired().HasMaxLength(1000);
+                entity.Property(r => r.Rating).IsRequired();
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
