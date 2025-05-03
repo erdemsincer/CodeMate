0f9ca5055a79aeff1807338c3c4c1915df223c3a
Erdem <erdemsincer123@icloud.com>
2025-05-03 04:06:33 +0300

    entity

--- ChatService/Entities/ChatMessage.cs
+++ ChatService/Entities/ChatMessage.cs
@@ -3,9 +3,14 @@
     public class ChatMessage
     {
         public int Id { get; set; }
+
         public Guid SenderId { get; set; }
         public Guid ReceiverId { get; set; }
-        public string Message { get; set; } = null!;
+
+        public string Content { get; set; } = null!;
+
         public DateTime SentAt { get; set; } = DateTime.UtcNow;
+
+        public bool IsRead { get; set; } = false;
     }
 }
